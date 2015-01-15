d2733eeced156866a74fc6d819bff2922454dda0
ed <ejbyne@gmail.com>
2015-01-15 18:45:28 +0000

    set up basic user crud routes

--- server.js
+++ server.js
@@ -27,9 +27,88 @@ app.get('/', function(req, res) {
 
 var apiRouter = express.Router();
 
+apiRouter.use(function(req, res, next) {
+  console.log('Somebody just came to our app!');
+  next();
+});
+
 apiRouter.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' });
 });
+
+apiRouter.route('/users')
+  
+  .post(function(req, res) {
+    var user = new User();
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.save(function(err) {
+      if (err) {
+        if (err.code == 11000) {
+          return res.json({ success: false, messaage: 'A user with that\
+ username already exists.' });
+        } else {
+          return res.send(err);
+        }
+      }
+      res.json({ message: 'User created!' });
+    });
+  })
+
+  .get(function(req, res) {
+    User.find(function(err, users) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(users);
+    });
+  });
+
+apiRouter.route('/users/:user_id')
+
+  .get(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) {
+        res.send(err);
+      }
+      res.json(user);
+    });
+  })
+
+  .put(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) {
+        res.send(err);
+      }
+      if (req.body.name) {
+        user.name = req.body.name;
+      }
+      if (req.body.username) {
+        user.username = req.body.username;
+      }
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+      user.save(function(err) {
+        if (err) {
+          res.send(err);
+        }
+        res.json({ message: 'User updated!' });
+      });
+    })
+  })
+
+  .delete(function(req, res) {
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user) {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
 
 app.use('/api', apiRouter);
 
